f7edb53d27da46cf49190b7bb6a732997cc8a2fe
himanshu9907 <himanshu05119@gmail.com>
2026-06-16 22:07:02 +0530

    Website updated

--- src/app/robots.js
+++ src/app/robots.js
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://evinsightshub.vercel.app/sitemap.xml",
+    sitemap: "https://evinsightshub.in/sitemap.xml",
   };
 }
